7f86438f6480c52eeff5907726b172a7c2acf3eb
Anton XPS <antonius.kravchuk@gmail.com>
2026-08-06 17:37:46 +0100

    adding faq command

--- cogs/communicate.py
+++ cogs/communicate.py
@@ -12,6 +12,7 @@ class Communicate(commands.Cog):
     dirname = os.path.split(cog_folder)[0]
     voicelines = os.path.join(dirname, "textfiles/voicelines.txt")
     lyrics = os.path.join(dirname, "textfiles/lyrics.txt")
+    faq_file = os.path.join(dirname, "textfiles/faq.txt")
 
     @discord.slash_command(name="hello", description="Say hi to our beloved PINAS:3")
     async def hello(self, ctx):
@@ -20,6 +21,11 @@ class Communicate(commands.Cog):
     @discord.slash_command(name="speak", description="See if PINAS can offer any words of wisdom")
     async def speak(self, ctx):
         await ctx.response.send_message(random.choice(open(self.lyrics).readlines()))
+
+    @discord.slash_command(name="faq", description="inFrequently Asked Questions")
+    async def faq(self, ctx):
+        a = open(self.faq_file).read()
+        await ctx.response.send_message(a, ephemeral = True)
 
 
 def setup(bot):
